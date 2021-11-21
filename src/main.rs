e4d8820f01ac7e2291f633c797344220e9c836d4
David Kern <david@mju.io>
2021-11-20 17:02:45 -0800

    define clap app

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::App;
+
+const CARGO_PKG_NAME: &'static str = env!("CARGO_PKG_NAME");
+const CARGO_PKG_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+const CARGO_PKG_DESCRIPTION: &'static str = env!("CARGO_PKG_DESCRIPTION");
+
+fn main() -> Result<()> {
+    // match program arguments
+    let _matches = App::new(CARGO_PKG_NAME)
+        .version(CARGO_PKG_VERSION)
+        .about(CARGO_PKG_DESCRIPTION)
+        .get_matches();
+
+    Ok(())
 }
